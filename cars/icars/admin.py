779fbcfa399d4803f9518f5775877f6756d3ec4a
Santi <sgatius93@gmail.com>
2016-04-15 13:01:27 +0200

    Afegint classe Marca, Any i Estil

--- cars/icars/admin.py
+++ cars/icars/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from icars.models import Model
+from icars.models import *
 # Register your models here.
 
 admin.site.register(Model)
+admin.site.register(Marca)
+admin.site.register(Year)
+admin.site.register(Estil)
