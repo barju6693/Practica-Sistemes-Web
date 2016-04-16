41c71af5d201782509b9690522d6aae6d0096a04
Davidcc1 <davidcc72@gmail.com>
2016-04-16 12:11:36 +0200

    creat /icars/urls.py i mod. de views + cars/urls.py

--- cars/icars/views.py
+++ cars/icars/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView
+from django.views.generic import DetailView, ListView
+from models import *
 
 # Create your views here.
 
 
 def mainpage(request):
     return HttpResponse('CARS')
+
+class MarcaList(ListView):
+    model = Marca
+
+class ModelList(ListView):
+    model = Model
+
+class EstilDetail(DetailView):
+    model = Estil
+    queryset = Estil.objects.order_by('year')[:10]
